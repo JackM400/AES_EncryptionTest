554a4a2507a860c8d51884bfa0a7307d1d5d8179
JackM400 <jack.millar400@gmail.com>
2019-08-15 22:08:32 +0100

    test message , encoded and encrypted , byte to string conversion

--- mainTests.py
+++ mainTests.py
@@ -1,9 +1,11 @@
 from cryptography.fernet import Fernet
 
+# string encryption + decryption
 # 1.key from testKey.key
 # 2.encode text
 # 3.encrypt text
-# 4 decrypt text
+# 4.decrypt text : encrypted message + key => original
+# 5. convert bytes to string
 
 # 1
 file = open('testKey.key')
@@ -16,8 +18,25 @@ encodedMessage = message.encode()
 # print(encodedMessage)
 
 # 3
-E = Fernet(key)
-encryptedMessage = E.encrypt(encodedMessage)
+encrypt = Fernet(key)
+encryptedMessage = encrypt.encrypt(encodedMessage)
 print(encryptedMessage)
+# print("\n")
 
 # 4
+# 4.1 get key
+file = open('testkey.key', "rb")
+newKey = file.read()  # key : bytes | should be same key as in 1
+file.close()
+# 4.2 decrypt encrypted message
+encrypt_2 = Fernet(key)
+decryptedMessage = encrypt_2.decrypt(encryptedMessage)
+
+print(decryptedMessage)  # still :bytes
+
+# 5
+originalMessage = decryptedMessage.decode()
+print(originalMessage)
+
+# file encryption + decryption
+
