200b55c452eab8a4d1f6933e641b6c5e15717335
JackM400 <jack.millar400@gmail.com>
2019-08-15 21:28:44 +0100

    required imports

--- encryption test.py
+++ encryption test.py
@@ -1,1 +1,6 @@
 import cryptography
+import os
+import base64
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
