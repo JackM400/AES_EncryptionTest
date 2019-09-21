1bfa1573a35b12f7e98e7019ad624a78dcd29abe
JackM400 <jack.millar400@gmail.com>
2019-09-21 20:24:22 +0100

    sample text file added, code access file

--- simplified.py
+++ simplified.py
@@ -1,6 +1,6 @@
 import cryptography.fernet as set
 
-#TODO
+# TODO
 # 1.generate base key ,
 # 2.get input message ,
 # 3.process message to encrypt ,
@@ -9,4 +9,11 @@ import cryptography.fernet as set
 # 4.decrypt message ,
 # act -on message
 
+# 1
+seed = set.Fernet.generate_key()
+key = set.Fernet(seed)
 
+# 2
+targetFile = open('simplified_text', "r")
+text = targetFile.read()
+print(text)
