5c599d1e78ba058afb3b491067e62e678b66c163
JackM400 <jack.millar400@gmail.com>
2019-09-21 20:05:11 +0100

    TODO for file

--- Simplified.py
+++ Simplified.py
@@ -1,1 +1,12 @@
 import cryptography.fernet as set
+
+#TODO
+# 1.generate base key ,
+# 2.get input message ,
+# 3.process message to encrypt ,
+# 3a.check encryption ,
+# transmit ,
+# 4.decrypt message ,
+# act -on message
+
+
